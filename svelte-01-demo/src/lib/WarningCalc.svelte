82c81b91920039d9a1285cfdefc24d2928d37fe1
Sosnus <sosnus15@gmail.com>
2023-07-12 06:26:49 +0200

    improved table

--- svelte-01-demo/src/lib/WarningCalc.svelte
+++ svelte-01-demo/src/lib/WarningCalc.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  let minError: number = 42
+  let maxError: number = 40
+  let maxWarning: number = 30
+  let virtualMaxOk : number = 0
+  let virtualMinOk : number = 0
   let minWarning: number = 0
-  let maxWarning: number = 0
-  let maxError: number = 0
+  let minError: number = -10
   const increment = () => {
     // count += 1
   }
@@ -20,31 +22,35 @@ color: gray;
 <!-- <button on:click={increment}>
   count is {minError}
 </button> -->
+<p>
+  Zakresy ostrzeżeń w formacie alertCondition[:warningConditon] (np. [-10]40:[0]30 )
+</p>
 <table>
   <tr>
     <td>ERROR</td>
-    <td><input value="-∞" class="lock"  readonly/><br><input bind:value={minError} /></td>
-  </tr>
-  <tr>
-    <td>WARNING</td>
-    <td><input bind:value={minError} /></td>
-  </tr>
-  <tr>
-    <td>OK</td>
-    <td>
-      <input value="-∞" class="lock"  readonly/><br>
-      <input bind:value={minError} class="lock"  />
-    </td>
-
-    <!-- <td><input bind:value={minError} /></td> -->
+    <td><input value="-∞" class="lock"  readonly/><br><input bind:value={maxError} /></td>
   </tr>
   <tr>
     <td>WARNING</td>
     <td><input bind:value={maxWarning} /></td>
   </tr>
   <tr>
+    <td>OK</td>
+    <td>
+      <input bind:value={virtualMaxOk} class="lock"  />
+      <br>
+      <input bind:value={virtualMinOk} class="lock"  />
+    </td>
+
+    <!-- <td><input bind:value={minError} /></td> -->
+  </tr>
+  <tr>
+    <td>WARNING</td>
+    <td><input bind:value={minWarning} /></td>
+  </tr>
+  <tr>
     <td>ERROR</td>
-    <td><input bind:value={maxError} /><br>
+    <td><input bind:value={minError} /><br>
       <input value="∞" class="lock" readonly/></td>
   </tr>
 </table>
